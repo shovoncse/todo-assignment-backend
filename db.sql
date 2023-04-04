8028410e3aabc12a6f5650956c6735a341229199
Shovon Das <shovon@brillmark.com>
2023-04-04 14:57:29 +0600

    db schema update

--- db.sql
+++ db.sql
@@ -3,16 +3,11 @@ drop database if exists todo;
 create database todo;
 use todo;
 
- create table task(
-    id serial int primary key,
-    description varchar(255) not null,
- );
+CREATE TABLE task (
+    id serial PRIMARY KEY,
+    description varchar(255) NOT NULL,
+    status boolean NOT NULL DEFAULT false
+);
  
 insert into task(description) values('Online Shopping');
 insert into task(description) values('Go to the gym');
-insert into task(description) values('Go to the bank');
-insert into task(description) values('Go to the doctor');
-insert into task(description) values('Go to the dentist');
-insert into task(description) values('Go to the hairdresser');
-insert into task(description) values('Go to the supermarket');
-insert into task(description) values('Go to the post office');
